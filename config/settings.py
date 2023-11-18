f06c3f4b1f50659453dd2efd3d05a32b157d1e97
WHAT <ygyshka0@gmail.com>
2023-11-18 16:08:52 +0700

    edit

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'django_celery_beat',
     'rest_framework',
+    'django_filters',
     'rest_framework_simplejwt',
     'corsheaders',
     
