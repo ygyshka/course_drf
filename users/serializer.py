823afe63252b89742deda35acc191fca6612385e
WHAT <ygyshka0@gmail.com>
2023-11-19 16:40:42 +0700

    add user model

--- users/serializer.py
+++ users/serializer.py
@@ -1,5 +1,25 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'phone', 'country', 'avatar', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+            return instance
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+
 )
 
 from users.apps import UsersConfig
+from users.views import UserRegister, UserListAPIView
 
 app_name = UsersConfig.name
 
@@ -12,5 +14,6 @@ urlpatterns = [
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('register/', UserRegister.as_view(), name='user_register'),
+    path('list/', UserListAPIView.as_view(), name='user-list')
 ]
